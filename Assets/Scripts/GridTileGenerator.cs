4824ff3b0af9bca2df0d8c223e0ce4289f01cb5f
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2021-03-16 21:15:52 -0400

    Add Graphs as Prefabs

--- Assets/Scripts/GridTileGenerator.cs
+++ Assets/Scripts/GridTileGenerator.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using UnityEngine.UIElements;
 
+// DISABLE THIS in the manager if the tile already exist!!
 public class GridTileGenerator : MonoBehaviour
 {
     [SerializeField] private float length;
@@ -12,7 +9,6 @@ public class GridTileGenerator : MonoBehaviour
 
     [SerializeField] private GameObject gridTilePrefab;
 
-    // Start is called before the first frame update
     void Start() => GenerateGridTiles();
 
     private void GenerateGridTiles()
